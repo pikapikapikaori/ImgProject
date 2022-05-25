7b53f432effee315a84f90814b29c9c5f4352475
pikapikapi <pikapikapi_kaori@icloud.com>
2022-05-25 18:56:04 +0800

    Beautify code

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import time
-
-import cv2
-import numpy as np
-
 import utils
 
 
@@ -10,13 +5,13 @@ def main():
     print("图像处理程序")
     print("-------------------")
     print("请选择要执行的操作")
-    operation = -1;
+    operation = -1
     while operation != 0:
         print("可选操作：\n0.结束程序\n1.图像基本操作\n2.图像直方图绘制与图像增强\n3.图像分割\n4.图像平滑与锐化\n5.图像形态学操作\n6.图像恢复\n7.年龄变换")
 
         try:
             operation = int(input("请选择要进行的基本操作（输入数字）："))
-        except ValueError as e:
+        except ValueError:
             print("输入错误，请重新输入：")
             continue
 
